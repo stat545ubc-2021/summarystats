0fed272395c13943d9070c58e5ad8e51ae6b1b1f
stuttle1 <90414345+stuttle1@users.noreply.github.com>
2021-11-18 21:12:09 -0800

    Update testthat.R
    Adding the tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,23 @@
 library(testthat)
-library(summarystats)
+library(palmerpenguins)
 
-test_check("summarystats")
+#' test 1: does an error occur when x is numeric?
+test_that("Invalid input for x", {
+  expect_error(summarystat(palmerpenguins::penguins, "bill_length_mm", "species"))
+})
+
+#' test 2: Is a specific error message produced when y is not numeric?
+test_that("error message is produced for x", {
+  expect_error(summarystat(palmerpenguins::penguins, "bill_length_mm", "species"), "x cannot be numeric. x must be a factor or column name")
+})
+
+#' test 3: Testing that the column names are right
+test_that("the column names are correct", {
+  expect_named(summarystat(palmerpenguins::penguins, "species", "bill_length_mm"), c("species",
+                                                                                     "mean", "median", "sd"))
+})
+
+#' test 4: testing that the table dimensions are right
+test_that("the number of columns are correct", {
+  expect_equal(4, ncol(summarystat(palmerpenguins::penguins, "species", "bill_length_mm")))
+})
